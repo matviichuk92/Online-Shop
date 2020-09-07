571d5dcfb45c9cb4f3af08ae4518f6d22c327cd1
Roman <matviichuk.office@gmail.com>
2020-09-07 19:36:21 +0300

    added new method in Application and @Override toString();

--- src/main/java/online/shop/Application.java
+++ src/main/java/online/shop/Application.java
@@ -2,7 +2,12 @@ package online.shop;
 
 import online.shop.lib.Injector;
 import online.shop.model.Product;
+import online.shop.model.ShoppingCart;
+import online.shop.model.User;
+import online.shop.service.OrderService;
 import online.shop.service.ProductService;
+import online.shop.service.ShoppingCartService;
+import online.shop.service.UserService;
 
 public class Application {
     private static Injector injector = Injector.getInstance("online.shop");
@@ -11,10 +16,34 @@ public class Application {
         ProductService productService = (ProductService) injector.getInstance(ProductService.class);
         Product whiskas = productService.create(new Product("Whiskas", 12.50));
         Product gourmet = productService.create(new Product("Gourmet", 23.50));
-        System.out.println("Get by Id: " + "\n" + productService.getById(gourmet.getId()));
-        productService.deleteById(gourmet.getId());
-        whiskas.setPrice(10.00);
-        productService.update(whiskas);
-        System.out.println("After update and delete: " + "\n" + productService.getAllProducts());
+
+        UserService userService = (UserService) injector.getInstance(UserService.class);
+        User roman = new User("Roman", "PapaCats", "cat123");
+        User mila = new User("Mila", "MamaCats", "123");
+        userService.create(roman);
+        userService.create(mila);
+        System.out.println("All users : " + userService.getAll());
+        mila.setPassword("Mila4TheBest");
+        userService.update(mila);
+        System.out.println("All users after update : " + userService.getAll());
+
+        ShoppingCartService shoppingCartService = (ShoppingCartService) injector
+                .getInstance(ShoppingCartService.class);
+        ShoppingCart cart = new ShoppingCart(roman.getId());
+        shoppingCartService.create(cart);
+        shoppingCartService.addProduct(cart, whiskas);
+        shoppingCartService.addProduct(cart, gourmet);
+        System.out.println("Shopping cart after add : "
+                + shoppingCartService.getByUserId(roman.getId()));
+        shoppingCartService.deleteProduct(cart, whiskas);
+        System.out.println("Shopping cart after delete : "
+                + shoppingCartService.getByUserId(roman.getId()));
+
+        OrderService orderService = (OrderService) injector.getInstance(OrderService.class);
+        orderService.completeOrder(cart);
+        System.out.println("Roman's order : " + orderService.getUserOrders(roman.getId()));
+        orderService.delete(1L);
+        System.out.println("Roman's order after delete : "
+                + orderService.getUserOrders(roman.getId()));
     }
 }
--- src/main/java/online/shop/model/ShoppingCart.java
+++ src/main/java/online/shop/model/ShoppingCart.java
@@ -36,4 +36,9 @@ public class ShoppingCart {
     public void setUserId(Long userId) {
         this.userId = userId;
     }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{" + "id=" + id + ", products=" + products + ", userId=" + userId + '}';
+    }
 }
